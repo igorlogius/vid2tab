c794c3b45a9417d6a2437194dbe4cbd65d9f0b4a
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-11-06 00:08:40 +0100

    approve video selecton

--- background.js
+++ background.js
@@ -7,24 +7,34 @@ function onError(e,m) {
 
 browser.menus.create({   // menus permission
 	id: extId, 
-	title: "Maximize Video in Tab",
+	title: "Toggle Video Size",
 	documentUrlPatterns: [ "<all_urls>" ],
 	contexts: ["all","video","page"],
 	onclick(info,tab) {
 		browser.tabs.executeScript(tab.id, {
 			frameId: info.frameId,
 			code:`
-				el = browser.menus.getTargetElement(${info.targetElementId});	
-				if(el.tagName === 'VIDEO') {
-					el.classList.toggle("vid2tab");
+				var el = browser.menus.getTargetElement(${info.targetElementId});
+				if(el !== null && el.tagName.toLowerCase() === 'video') {
 					document.body.classList.toggle("hide");
+					el.classList.toggle("vid2tab");
 					el.controls=true;
 				}else{
-					els = el.querySelectorAll('video');
-					if(els.length > 0){
-						els[0].classList.toggle("vid2tab");
-						document.body.classList.toggle("hide");
-						els[0].controls=true;
+					el = el.querySelectorAll('video');
+					if(el.length > 0){
+						el = el[0];
+						if(el !== null && el.tagName.toLowerCase() === 'video'){
+							document.body.classList.toggle("hide");
+							el.classList.toggle("vid2tab");
+							el.controls=true;
+						}else{
+							el = el.closest('video');
+							if(el !== null && el.tagName.toLowerCase() === 'video'){
+								document.body.classList.toggle("hide");
+								el.classList.toggle("vid2tab");
+								els[0].controls=true;
+							}
+						}
 					}
 				}
 			`,
